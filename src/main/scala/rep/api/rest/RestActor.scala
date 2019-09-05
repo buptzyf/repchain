ed197346da5a24a31ba2c1803b237c39732eab55
wuwei1972 <jiangbuyun@sina.com>
2019-09-05 17:31:05 +0800

    修改源码格式

--- src/main/scala/rep/api/rest/RestActor.scala
+++ src/main/scala/rep/api/rest/RestActor.scala
@@ -47,11 +47,11 @@ import rep.sc.TypeOfSender
 import rep.sc.SandboxDispatcher.DoTransaction
 import rep.sc.Sandbox.DoTransactionResult
 /**
-  * RestActor伴生object，包含可接受的传入消息定义，以及处理的返回结果定义。
-  * 以及用于建立Tranaction，检索Tranaction的静态方法
-  * @author c4w created
-  *
-  */
+ * RestActor伴生object，包含可接受的传入消息定义，以及处理的返回结果定义。
+ * 以及用于建立Tranaction，检索Tranaction的静态方法
+ * @author c4w created
+ *
+ */
 
 object RestActor {
   def props(name: String): Props = Props(classOf[RestActor], name)
@@ -77,41 +77,41 @@ object RestActor {
           iptFunc: String, iptArgs: Seq[String], timeout: Int,
           secureContext: String, code: String, ctype: Int)    */
   case class CSpec(stype: Int, chaincodename: String, chaincodeversion: Int,
-                   iptFunc: String, iptArgs: Seq[String], timeout: Int,legal_prose:String,
+                   iptFunc: String, iptArgs: Seq[String], timeout: Int, legal_prose: String,
                    code: String, ctype: Int, state: Boolean)
   case class tranSign(tran: String)
 
   /**
-    * 根据节点名称和chainCode定义建立交易实例
-    * @param nodeName 节点名称
-    * @param c chainCode定义
-    */
+   * 根据节点名称和chainCode定义建立交易实例
+   * @param nodeName 节点名称
+   * @param c chainCode定义
+   */
   def buildTranaction(nodeName: String, c: CSpec): Transaction = {
     val stype = c.stype match {
       case 1 =>
         Transaction.Type.CHAINCODE_DEPLOY
       case 2 =>
         Transaction.Type.CHAINCODE_INVOKE
-      case 3=>
+      case 3 =>
         Transaction.Type.CHAINCODE_SET_STATE
       case _ =>
         Transaction.Type.UNDEFINED
     }
-    val ctype = c.ctype match{
+    val ctype = c.ctype match {
       case 2 =>
         rep.protos.peer.ChaincodeDeploy.CodeType.CODE_SCALA
-      case 3 =>  
+      case 3 =>
         rep.protos.peer.ChaincodeDeploy.CodeType.CODE_SCALA_PARALLEL
       case _ =>
         rep.protos.peer.ChaincodeDeploy.CodeType.CODE_JAVASCRIPT
     }
 
-    val chaincodeId = new ChaincodeId(c.chaincodename,c.chaincodeversion)
-    if(stype==Transaction.Type.CHAINCODE_DEPLOY){
+    val chaincodeId = new ChaincodeId(c.chaincodename, c.chaincodeversion)
+    if (stype == Transaction.Type.CHAINCODE_DEPLOY) {
       PeerHelper.createTransaction4Deploy(nodeName, chaincodeId, c.code, c.legal_prose, c.timeout, ctype)
-    }else if(stype==Transaction.Type.CHAINCODE_INVOKE){
+    } else if (stype == Transaction.Type.CHAINCODE_INVOKE) {
       PeerHelper.createTransaction4Invoke(nodeName, chaincodeId, c.iptFunc, c.iptArgs)
-    }else if (stype == Transaction.Type.CHAINCODE_SET_STATE){
+    } else if (stype == Transaction.Type.CHAINCODE_SET_STATE) {
       PeerHelper.createTransaction4State(nodeName, chaincodeId, c.state)
     } else {
       null
@@ -121,14 +121,14 @@ object RestActor {
 }
 
 /**
-  * RestActor负责处理rest api请求
-  *
-  */
-class RestActor(moduleName: String) extends  ModuleBase(moduleName) {
+ * RestActor负责处理rest api请求
+ *
+ */
+class RestActor(moduleName: String) extends ModuleBase(moduleName) {
 
   import RestActor._
   import spray.json._
-  import akka.http.scaladsl.model.{HttpResponse, MediaTypes,HttpEntity}
+  import akka.http.scaladsl.model.{ HttpResponse, MediaTypes, HttpEntity }
 
   //import rep.utils.JsonFormat.AnyJsonFormat
 
@@ -136,48 +136,48 @@ class RestActor(moduleName: String) extends  ModuleBase(moduleName) {
   val sr: ImpDataAccess = ImpDataAccess.GetDataAccess(pe.getSysTag)
 
   // 先检查交易大小，然后再检查交易是否已存在，再去验证签名，如果没有问题，则广播
-  def preTransaction(t:Transaction) : Unit ={
-    val tranLimitSize = SystemProfile.getBlockLength/3
+  def preTransaction(t: Transaction): Unit = {
+    val tranLimitSize = SystemProfile.getBlockLength / 3
     if (t.toByteArray.length > tranLimitSize) {
       sender ! PostResult(t.id, None, Option(s"交易大小超出限制： ${tranLimitSize}，请重新检查"))
     }
-    
-    if (pe.getNodeMgr.getStableNodes.size < SystemProfile.getVoteNoteMin)  {
-      sender ! PostResult(t.id, None, Option("系统共识节点数目太少，暂时无法处理交易"))
+
+    if (pe.getNodeMgr.getStableNodes.size < SystemProfile.getVoteNoteMin) {
+      sender ! PostResult(t.id, None, Option("共识节点数目太少，暂时无法处理交易"))
     }
-    
+
     val sig = t.signature.get.signature.toByteArray
     val tOutSig = t.clearSignature
     val certId = t.signature.get.certId.get
-    try{
+    try {
       sr.getTransDataByTxId(t.id) match {
         case st: Some[Transaction] =>
           sender ! PostResult(t.id, None, Option(s"transactionId is exists, the transaction is \n ${JsonFormat.toJson(st.get)}"))
         case None =>
-          if (SignTool.verify(sig, tOutSig.toByteArray, certId,pe.getSysTag)) {
-              val future = pe.getActorRef(ActorType.transactiondispatcher) ? DoTransaction(t, "api_" + t.id,TypeOfSender.FromAPI)
-              val result = Await.result(future, timeout.duration).asInstanceOf[DoTransactionResult]
-              val rv = result
-              // 释放存储实例
-              ImpDataPreloadMgr.Free(pe.getSysTag,t.id)
-              rv.err match {
-                case None =>
-                  //预执行正常,提交并广播交易
-                  pe.getActorRef(ActorType.transactionpool) ! t // 给交易池发送消息 ！=》告知（getActorRef）
-                  if (rv.r == null)
-                    sender ! PostResult(t.id, None, None)
-                  else
-                    sender ! PostResult(t.id, Some(rv.r), None)  // legal_prose need
-                case Some(err) =>
-                  //预执行异常,废弃交易，向api调用者发送异常
-                  sender ! PostResult(t.id, None, Option(err.cause.getMessage))
-              }
+          if (SignTool.verify(sig, tOutSig.toByteArray, certId, pe.getSysTag)) {
+            val future = pe.getActorRef(ActorType.transactiondispatcher) ? DoTransaction(t, "api_" + t.id, TypeOfSender.FromAPI)
+            val result = Await.result(future, timeout.duration).asInstanceOf[DoTransactionResult]
+            val rv = result
+            // 释放存储实例
+            ImpDataPreloadMgr.Free(pe.getSysTag, t.id)
+            rv.err match {
+              case None =>
+                //预执行正常,提交并广播交易
+                pe.getActorRef(ActorType.transactionpool) ! t // 给交易池发送消息 ！=》告知（getActorRef）
+                if (rv.r == null)
+                  sender ! PostResult(t.id, None, None)
+                else
+                  sender ! PostResult(t.id, Some(rv.r), None) // legal_prose need
+              case Some(err) =>
+                //预执行异常,废弃交易，向api调用者发送异常
+                sender ! PostResult(t.id, None, Option(err.cause.getMessage))
+            }
           } else {
             sender ! PostResult(t.id, None, Option("验证签名出错"))
           }
       }
-    }catch{
-      case e : RuntimeException =>
+    } catch {
+      case e: RuntimeException =>
         sender ! PostResult(t.id, None, Option(e.getMessage))
     }
   }
@@ -191,7 +191,7 @@ class RestActor(moduleName: String) extends  ModuleBase(moduleName) {
         txr = Transaction.parseFrom(tr1)
         preTransaction(txr)
       } catch {
-        case e:Exception =>
+        case e: Exception =>
           sender ! PostResult(txr.id, None, Option(s"transaction parser error! + ${e.getMessage}"))
       }
 
@@ -199,11 +199,11 @@ class RestActor(moduleName: String) extends  ModuleBase(moduleName) {
     case c: CSpec =>
       var txr = Transaction.defaultInstance
       //debug状态才动用节点密钥签名
-      if(contractOperationMode==0){
+      if (contractOperationMode == 0) {
         //构建transaction并通过peer预执行广播
         txr = buildTranaction(pe.getSysTag, c)
         preTransaction(txr)
-      }else
+      } else
         sender ! PostResult(txr.id, None, Option("非Debug状态下此调用无效"))
 
     // 流式提交交易
@@ -228,9 +228,6 @@ class RestActor(moduleName: String) extends  ModuleBase(moduleName) {
       }
       sender ! r
 
-
-   
-
     // 根据高度检索块
     case BlockHeight(h) =>
       val bb = sr.getBlockByHeight(h)
@@ -246,7 +243,7 @@ class RestActor(moduleName: String) extends  ModuleBase(moduleName) {
     case BlockHeightStream(h) =>
       val bb = sr.getBlockByHeight(h)
       if (bb == null) {
-        sender ! HttpResponse(entity = HttpEntity.Strict(MediaTypes.`application/octet-stream`,akka.util.ByteString.empty))
+        sender ! HttpResponse(entity = HttpEntity.Strict(MediaTypes.`application/octet-stream`, akka.util.ByteString.empty))
       } else {
         val body = akka.util.ByteString(bb)
         val entity = HttpEntity.Strict(MediaTypes.`application/octet-stream`, body)
@@ -267,7 +264,7 @@ class RestActor(moduleName: String) extends  ModuleBase(moduleName) {
 
     // 根据txid检索交易
     case TransactionId(txId) =>
-      var r = sr.getTransDataByTxId( txId) match {
+      var r = sr.getTransDataByTxId(txId) match {
         case None =>
           QueryResult(None)
         case t: Some[Transaction] =>
@@ -275,12 +272,11 @@ class RestActor(moduleName: String) extends  ModuleBase(moduleName) {
       }
       sender ! r
 
-
     // 根据txid检索交易字节流
     case TransactionStreamId(txId) =>
-      val r = sr.getTransDataByTxId( txId)
+      val r = sr.getTransDataByTxId(txId)
       if (r.isEmpty) {
-        sender ! HttpResponse(entity = HttpEntity.Strict(MediaTypes.`application/octet-stream`,akka.util.ByteString.empty))
+        sender ! HttpResponse(entity = HttpEntity.Strict(MediaTypes.`application/octet-stream`, akka.util.ByteString.empty))
       } else {
         val t = r.get
         val body = akka.util.ByteString(t.toByteArray)
