64dd19a46b01e9d9990e2d641b0ef36be5647b58
wuwei1972 <jiangbuyun@sina.com>
2019-11-15 13:45:33 +0800

    还原交易查重代码

--- src/main/scala/rep/network/tools/transpool/TransactionPoolMgr.scala
+++ src/main/scala/rep/network/tools/transpool/TransactionPoolMgr.scala
@@ -33,7 +33,7 @@ class TransactionPoolMgr {
   
   private implicit var transactions = new ConcurrentSkipListMap[Long,Transaction]() asScala
   private implicit var transKeys = new ConcurrentHashMap[String,Long]() asScala
-  //private implicit var transNumber = new AtomicInteger(0) 
+  private implicit var transNumber = new AtomicInteger(0) 
   
   def getTransListClone(start:Int,num: Int,sysName:String): Seq[Transaction] = {
     var translist = scala.collection.mutable.ArrayBuffer[Transaction]()
@@ -49,16 +49,16 @@ class TransactionPoolMgr {
         transactions.foreach(f=>{
           if(count <= num){
             val txid = f._2.id
-            /*if ((currenttime - f._1/1000000000) > TimePolicy.getTranscationWaiting || sr.isExistTrans4Txid(txid) ){
+            if ((currenttime - f._1/1000000000) > TimePolicy.getTranscationWaiting || sr.isExistTrans4Txid(txid) ){
               deltrans4id += txid
-            }else{*/
+            }else{
               if(pos < start){
                 pos += 1
               }else{
                 translist += f._2
                 count += 1
               }
-            //}
+            }
           }else{
             break
           }
@@ -69,7 +69,7 @@ class TransactionPoolMgr {
           deltrans4id.foreach(f=>{
             RepLogger.info(RepLogger.TransLifeCycle_Logger,  s"systemname=${sysName},remove trans from pool,trans timeout or exist in block,${f}")
             removeTranscation4Txid(f,sysName)
-            //transNumber.addAndGet(-1)
+            transNumber.addAndGet(-1)
           })
         }
         deltrans4id.clear()
@@ -94,7 +94,7 @@ class TransactionPoolMgr {
       }else{
         transactions.put(time, tran)
         transKeys.put(txid, time)
-        //transNumber.addAndGet(1)
+        transNumber.addAndGet(1)
         RepLogger.info(RepLogger.TransLifeCycle_Logger,  s"systemname=${sysName},trans entry pool,${tran.id},entry time = ${time}")
       }
     }finally {
@@ -120,7 +120,7 @@ class TransactionPoolMgr {
     try{
       trans.foreach(f=>{
         removeTranscation(f,sysName)
-        //transNumber.addAndGet(-1)
+        transNumber.addAndGet(-1)
       })
     }finally{
         transLock.unlock()
@@ -155,7 +155,7 @@ class TransactionPoolMgr {
   }
 
   def getTransLength() : Int = {
-    var len = 0
+    /*var len = 0
     transLock.lock()
     val start = System.currentTimeMillis()
     try{
@@ -165,7 +165,7 @@ class TransactionPoolMgr {
     }
     val end = System.currentTimeMillis()
     RepLogger.trace(RepLogger.OutputTime_Logger, s"getTransLength spent time=${end-start}")
-    len
-    //this.transNumber.get
+    len*/
+    this.transNumber.get
   }
 }
