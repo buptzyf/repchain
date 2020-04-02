db713450bdc4cd6b69af382ba18f3f689f4d3778
zhaohj@sina.com <81799935>
2020-04-02 09:59:20 +0800

    forgot one file

---
+++ src/main/scala/rep/network/consensus/common/algorithm/ISequencialAlgorithmOfVote.scala
@@ -0,0 +1,37 @@
+package rep.network.consensus.common.algorithm
+
+import rep.log.RepLogger
+import rep.storage.util.pathUtil
+
+import scala.math.pow
+
+/**
+ * Created by jiangbuyun on 2020/03/17.
+ * 实现随机抽签的算法
+ */
+
+class ISequencialAlgorithmOfVote extends IAlgorithmOfVote {
+  case class randomNumber(var number:Long,var generateSerial:Int)
+
+  override def blocker(nodes: Array[String], position:Int): String = {
+    if(nodes.nonEmpty){
+      var pos = position
+      if(position >= nodes.size){
+        pos = position % nodes.size
+      }
+      nodes(pos)
+    }else{
+      null
+    }
+  }
+
+  override def candidators(Systemname:String,hash:String,nodes: Set[String], seed: Array[Byte]): Array[String] = {
+    var nodesSeq = nodes.toSeq.sortBy(f => (f))
+    var len = nodes.size
+    var candidate = new Array[String](len)
+    for (j <- 0 until len) {
+      candidate(j) = nodesSeq(j)
+    }
+    candidate
+  }
+}
