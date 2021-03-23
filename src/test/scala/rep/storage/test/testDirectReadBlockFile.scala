75982315de6fde6c7312df17ced9bfdeed7a73cc
jiangbuyun <jiangbuyun@sina.com>
2021-03-23 17:49:55 +0800

    添加直接读区块文件的类和测试类。

---
+++ src/test/scala/rep/storage/test/testDirectReadBlockFile.scala
@@ -0,0 +1,20 @@
+package rep.storage.test
+
+import rep.storage.block.BlockFileDirectReader
+
+object testDirectReadBlockFile extends App{
+  val reader = new BlockFileDirectReader("/Users/jiangbuyun/Repchain_BlockFile_50.log")
+  val block = reader.readBlockWithHeight(10192214)
+  System.out.println(block)
+
+  //readBlockFile
+
+  def readBlockFile()={
+    var result = reader.IterationBlock(0)
+    while(result._1 != null){
+      System.out.println("read block data,block height="+result._1.height)
+      result = reader.IterationBlock(result._2)
+    }
+    reader.FreeResouce
+  }
+}
