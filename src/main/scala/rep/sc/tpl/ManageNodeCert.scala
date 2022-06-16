806a0eefd25a6b5b9df953c425877d0369639af9
brightestboy <yafeng@iscas.ac.cn>
2022-06-16 15:38:40 +0800

    在ManageNodeCert合约中增加更新抽签列表的方法

--- src/main/scala/rep/sc/tpl/ManageNodeCert.scala
+++ src/main/scala/rep/sc/tpl/ManageNodeCert.scala
@@ -1,11 +1,12 @@
 package rep.sc.tpl
 
-import java.io.StringReader
 import org.bouncycastle.util.io.pem.PemReader
 import org.json4s._
 import org.json4s.jackson.JsonMethods._
 import rep.proto.rc2.ActionResult
 import rep.sc.scalax.{ContractContext, ContractException, IContract}
+
+import java.io.StringReader
 import scala.collection.mutable.HashMap
 
 
@@ -15,12 +16,14 @@ import scala.collection.mutable.HashMap
 class ManageNodeCert extends IContract {
 
   val key_trust_stores = "TSDb-Trust-Stores"
+  val vote_list = "Vote-List"
 
   def init(ctx: ContractContext) {
     println(s"tid: ${ctx.t.id}, execute the contract which name is ${ctx.t.getCid.chaincodeName} and version is ${ctx.t.getCid.version}")
   }
 
   /**
+   * 管理组网节点的信任证书列表
    *
    * @param ctx
    * @param data (节点名 -> 证书pem字符串)，如果是(节点名 -> "")，则移除该节点的证书
@@ -28,7 +31,7 @@ class ManageNodeCert extends IContract {
    */
   def updateNodeCert(ctx: ContractContext, data: Map[String, String]): ActionResult = {
     if (ctx.api.getVal(key_trust_stores) == null) {
-      throw ContractException("未初始化")
+      throw ContractException("组网节点信任证书列表未初始化")
     }
     val certMap = ctx.api.getVal(key_trust_stores).asInstanceOf[HashMap[String, Array[Byte]]]
     for ((alias, certPem) <- data) {
@@ -45,6 +48,22 @@ class ManageNodeCert extends IContract {
   }
 
   /**
+   * 更新投票列表
+   *
+   * @param ctx
+   * @param voteList 抽签列表
+   * @return
+   */
+  def updateVoleList(ctx: ContractContext, voteList: List[String]): ActionResult = {
+    if (voteList.size >= 4) {
+      ctx.api.setVal(vote_list, voteList)
+    } else {
+      throw ContractException("抽签列表长度至少为4")
+    }
+    null
+  }
+
+  /**
    * 根据action,找到对应的method，并将传入的json字符串parse为method需要的传入参数
    */
   def onAction(ctx: ContractContext, action: String, sdata: String): ActionResult = {
@@ -55,6 +74,8 @@ class ManageNodeCert extends IContract {
     action match {
       case "updateNodeCert" =>
         updateNodeCert(ctx, json.extract[Map[String, String]])
+      case "updateVoteList" =>
+        updateVoleList(ctx, json.extract[List[String]])
     }
   }
 
