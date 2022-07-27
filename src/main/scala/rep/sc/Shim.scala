c7d4106608fcef3d33bb0e4ce2f48da6ea8e69cb
robinfang <thefmajia@gmail.com>
2022-07-28 01:14:44 +0800

    fix: getDIDSigner取错合约

--- src/main/scala/rep/sc/Shim.scala
+++ src/main/scala/rep/sc/Shim.scala
@@ -114,8 +114,10 @@ class Shim {
     if (key.indexOf("_") >= 0) {
       if (!key.equalsIgnoreCase(IdTool.getCid(t.getCid))
         && !key.equalsIgnoreCase(IdTool.getCid(t.getCid) + SandboxDispatcher.PRE_STATE)
-        && key.lastIndexOf("super_admin") < 0)
+        && key.lastIndexOf("super_admin") < 0) {
+        println("key: " + key + ", " + SandboxDispatcher.ERR_WORLDSTATE_CANNOT_CONTAIN_UNDERSCORES)
         throw new Exception(SandboxDispatcher.ERR_WORLDSTATE_CANNOT_CONTAIN_UNDERSCORES)
+      }
     }
   }
 
@@ -124,7 +126,7 @@ class Shim {
     setState(key, serialise(value))
   }
 
-  def delVal(key:Key):Unit={
+  def delVal(key: Key): Unit = {
     checkKeyName(key)
     delState(key)
   }
@@ -138,22 +140,22 @@ class Shim {
       deserialise(v)
   }
 
-  private def delState(key:Key):Unit={
+  private def delState(key: Key): Unit = {
     val pkey = pre_key + PRE_SPLIT + key
     var bs = ByteString.EMPTY
     if (this.stateSet.contains(pkey)) {
       //该状态字被删除之前已经发生更新并且是最后的更新，需要删除这个更新
       this.stateSet -= pkey
-    }else{
+    } else {
       checkStateGet(pkey)
     }
     bs = this.stateGet(pkey)
-    this.stateDel += pkey->bs
+    this.stateDel += pkey -> bs
     //执行删除操作
-    this.srOfTransaction.del(pkey,bs.toByteArray)
+    this.srOfTransaction.del(pkey, bs.toByteArray)
   }
 
-  private def checkStateGet(pkey:Key):Unit={
+  private def checkStateGet(pkey: Key): Unit = {
     if (!this.stateGet.contains(pkey)) {
       //如果该键从来没有read，从DB获取read，并写入到read日志
       val oldValue = get(pkey)
@@ -178,7 +180,7 @@ class Shim {
       this.srOfTransaction.put(pkey, nv.toByteArray)
       this.stateSet += pkey -> nv
     }
-    if(this.stateDel.contains(pkey)) this.stateDel -= pkey
+    if (this.stateDel.contains(pkey)) this.stateDel -= pkey
   }
 
   private def get(key: Key): Array[Byte] = {
@@ -226,9 +228,19 @@ class Shim {
    * @category 根据账户的DID标识获取完整账户信息
    * @param creditCode :String 账户的DID标识
    * @return Signer 账户完整信息
-   **/
-  def getDIDSigner(creditCode:String):Signer={
-    val tmpSigner = this.getVal(signerPrefix + creditCode)
+   * */
+  def getDIDSigner(creditCode: String): Signer = {
+    def specialGetVal(key: Key): Any = {
+      checkKeyName(key)
+      val somePreKey = KeyPrefixManager.getWorldStateKeyPrefix(ctx.getConfig, "RdidOperateAuthorizeTPL", "")
+      val v = get(somePreKey + PRE_SPLIT + key)
+      if (v == null)
+        null
+      else
+        deserialise(v)
+    }
+
+    val tmpSigner = specialGetVal(signerPrefix + creditCode)
     // 判断是否有值
     if (tmpSigner != null && tmpSigner.isInstanceOf[Signer]) {
       tmpSigner.asInstanceOf[Signer]
@@ -243,10 +255,10 @@ class Shim {
    * @since 2022-07-22
    * @category 根据账户的DID标识和证书名称获取证书信息
    * @param creditCode :String 账户的DID标识
-   * @param certName :String 证书名称
+   * @param certName   :String 证书名称
    * @return Certificate 证书信息
-   **/
-  def getSignerCert(creditCode:String,certName:String):Certificate={
+   * */
+  def getSignerCert(creditCode: String, certName: String): Certificate = {
     val tmpCert = this.getVal(certPrefix + creditCode + "." + certName)
     // 判断是否有值
     if (tmpCert != null && tmpCert.isInstanceOf[Certificate]) {
@@ -261,13 +273,13 @@ class Shim {
    * @version 2.0
    * @since 2022-07-22
    * @category 验证签名结果是否正确
-   * @param originalContent:Array[Byte] 签名的原始信息
-   * @param signatureResults:Array[Byte] 签名结果
-   * @param certInfo:CertId  账户证书标识
+   * @param originalContent  :Array[Byte] 签名的原始信息
+   * @param signatureResults :Array[Byte] 签名结果
+   * @param certInfo         :CertId  账户证书标识
    * @return Boolean 验证结果，true 验证通过；false 验证不通过
-   **/
-  def VerifySignature(originalContent:Array[Byte], signatureResults:Array[Byte],certInfo:CertId):Boolean={
-    this.ctx.getSignTool.verify(signatureResults,originalContent,certInfo)
+   * */
+  def VerifySignature(originalContent: Array[Byte], signatureResults: Array[Byte], certInfo: CertId): Boolean = {
+    this.ctx.getSignTool.verify(signatureResults, originalContent, certInfo)
   }
 
   //判断账号是否节点账号 TODO
@@ -337,27 +349,27 @@ class Shim {
   }
 
   @Deprecated
-  def isExistKey4DID(key:String):Boolean={
+  def isExistKey4DID(key: String): Boolean = {
     val baseKey = KeyPrefixManager.getCustomNetKeyPrefix(ctx.getConfig.getIdentityNetName, ctx.getConfig.getAccountContractName) + PRE_SPLIT + key
     val b = get(baseKey)
-    if(b == null){
-      if(ctx.getConfig.getIdentityNetName.equalsIgnoreCase(ctx.getConfig.getChainNetworkId)){
+    if (b == null) {
+      if (ctx.getConfig.getIdentityNetName.equalsIgnoreCase(ctx.getConfig.getChainNetworkId)) {
         false
-      }else{
-        val businessKey = KeyPrefixManager.getCustomNetKeyPrefix(ctx.getConfig.getChainNetworkId, ctx.getConfig.getAccountContractName)+ PRE_SPLIT + key
+      } else {
+        val businessKey = KeyPrefixManager.getCustomNetKeyPrefix(ctx.getConfig.getChainNetworkId, ctx.getConfig.getAccountContractName) + PRE_SPLIT + key
         val bs = get(businessKey)
-        if(bs == null){
+        if (bs == null) {
           false
-        }else{
+        } else {
           true
         }
       }
-    }else{
+    } else {
       true
     }
   }
 
-  def permissionCheck(did:String,certName:String,op:String):Boolean={
-    this.ctx.getPermissionVerify.CheckPermission(did,certName,op,this.srOfTransaction.getBlockPreload)
+  def permissionCheck(did: String, certName: String, op: String): Boolean = {
+    this.ctx.getPermissionVerify.CheckPermission(did, certName, op, this.srOfTransaction.getBlockPreload)
   }
 }
